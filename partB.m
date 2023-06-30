2f80466e0cb10543c792057a898cbcb8cbe081b6
SondosAFarrah <132385970+SondosAFarrah@users.noreply.github.com>
2023-06-30 16:10:49 +0300

    partB.m

--- partB.m
+++ partB.m
@@ -10,18 +10,28 @@ subplot(2, 2, 1);
 plot(css);
 xlabel('Time');
 ylabel('Amplitude');
-title('css');
+title('Composite Source Siganl');
 
 subplot(2, 2, 2);
-nfft = 2^nextpow2(length(css));
-Pxx = abs(fft(css,nfft)).^2/length(css)/fs;
-Hpsd = dspdata.psd(Pxx(1:length(Pxx)/2),'Fs',fs);  
-plot(Hpsd);
+[psd, freq] = pwelch(css, [], [], [], fs);
+plot(freq, 10*log10(psd));
+title('Power Spectral Density ');
+xlabel('Frequency (Hz)');
+ylabel('Power (dB)');
 
 subplot(2, 2, 3);
 Xc= [css css css css css];
+yy = linspace(0, (length(Xc)-1)*(1000/fs), length(Xc));
+length(Xc)
+length(yy)
 plot(Xc);
+title('Far-end Signal');
+xlabel('Time');
+ylabel('Amplitude');
 
 subplot(2, 2, 4);
 y = conv(path,Xc);
 plot(y);
+title('Echo Signal');
+xlabel('Time');
+ylabel('Amplitude');
