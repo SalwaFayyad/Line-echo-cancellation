6451dda5638d3956cb2a339f29bcb070fada36e6
SalwaFayyad <104863637+SalwaFayyad@users.noreply.github.com>
2023-07-02 00:53:12 +0300

    Update partB.m

--- partB.m
+++ partB.m
@@ -1,3 +1,4 @@
+%Salwa Fayyad 1200430 , Sondos Farrah 1200905 , Katya Kobari 1201478
 close all;
 clear all;
 clc;
@@ -14,24 +15,28 @@ title('Composite Source Siganl');
 
 subplot(2, 2, 2);
 [psd, freq] = pwelch(css, [], [], [], fs);
-plot(freq, 10*log10(psd));
+plot(freq, 10*log10(psd),'r');
 title('Power Spectral Density ');
 xlabel('Frequency (Hz)');
 ylabel('Power (dB)');
 
+%Far-end signal
 subplot(2, 2, 3);
 Xc= [css css css css css];
 yy = linspace(0, (length(Xc)-1)*(1000/fs), length(Xc));
 length(Xc)
 length(yy)
-plot(Xc);
+plot(Xc,'g');
 title('Far-end Signal');
 xlabel('Time');
 ylabel('Amplitude');
 
+
 subplot(2, 2, 4);
 y = conv(path,Xc);
-plot(y);
+plot(y,'m');
 title('Echo Signal');
 xlabel('Time');
 ylabel('Amplitude');
+
+
