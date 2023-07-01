9b34581825e4f1a676126522bbb6019674232585
SalwaFayyad <104863637+SalwaFayyad@users.noreply.github.com>
2023-07-02 00:53:28 +0300

    Update partC.m

--- partC.m
+++ partC.m
@@ -1,3 +1,4 @@
+%Salwa Fayyad 1200430 , Sondos Farrah 1200905 , Katya Kobari 1201478
 
 close all;
 clear all;
@@ -6,9 +7,9 @@ load('path.mat');
 load('css.mat');
 
 Xc = [css,css ,css ,css, css];
-N= length(Xc);
+N= length(Xc);%5600*5
 y = conv(path, Xc);
 
-input_power = 10 * log10((1/N) * sum(abs(Xc).^2))
-output_power = 10 * log10((1/N) * sum(abs(y).^2))
-ERL = output_power-input_power
+InputPower = 10 * log10((1/N) * sum(abs(Xc).^2))
+OutputPower = 10 * log10((1/N) * sum(abs(y).^2))
+ERL = OutputPower-InputPower
