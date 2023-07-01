4b11e7e318e5727d3b399e210c8b47ef05df9fbb
SalwaFayyad <104863637+SalwaFayyad@users.noreply.github.com>
2023-07-02 00:52:55 +0300

    Update partA.m

--- partA.m
+++ partA.m
@@ -1,7 +1,8 @@
+%Salwa Fayyad 1200430 , Sondos Farrah 1200905 , Katya Kobari 1201478
 load('path.mat');
 
 subplot(2, 1, 1);
-plot(path);
+plot(path,'m','LineWidth',2);
 xlabel('Time');
 ylabel('Amplitude');
 title('Impulse Response of Echo Path');
@@ -10,7 +11,7 @@ subplot(2, 1, 2);
 [H, w] = freqz(path, 1, length(path));
 magnitude_dB = 20 * log10(abs(H)); % Compute magnitude in dB
 
-plot(w/pi*4000, magnitude_dB);
-xlabel('Frequency (\times \pi radians/sample)');
-ylabel('Magnitude (dB)');
-title('Frequency Response (dB)');
+plot(w/pi*4000, magnitude_dB,'c','LineWidth',2);
+xlabel('Frequency');
+ylabel('Amplitude (dB)');
+title('Frequency Response ');
