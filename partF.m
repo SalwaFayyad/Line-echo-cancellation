abd93e6c54fad852f31e9fbb7aefc455292c50dc
SondosAFarrah <132385970+SondosAFarrah@users.noreply.github.com>
2023-07-02 19:24:31 +0300

    partF.m

--- partF.m
+++ partF.m
@@ -1,3 +1,4 @@
+%Salwa Fayyad 1200430 , Sondos Farrah 1200905 , Katya Kobari 1201478
 close all;
 clear all;
 clc;
@@ -5,36 +6,34 @@ clc;
 load('path.mat');
 load('css.mat');
 
-far_end_signal = repmat(css, 1, 10); % Repeat CSS data for 10 blocks
-echo_signal = filter(path, 1, far_end_signal);
+farEnd = repmat(css, 1, 10); 
+echoSignal = filter(path, 1, farEnd);
 
-filter_length = 128; % Number of filter taps
-step_size = 1e-6; % LMS step size
-adapt_filter = zeros(filter_length, 1); % Initialize adaptive filter coefficients
-delayedFarEndSignal = zeros(filter_length, 1);  
+filter_length = 128; 
+step_size = 1e-6; 
+adaptiveFilter = zeros(filter_length, 1); 
+delayedFarEnd = zeros(filter_length, 1);  
 
-% Initialize error signal with the size of far_end_signal
-error_signal = zeros(size(far_end_signal));
-estimatedEchoPath = zeros(size(far_end_signal));
+error_signal = zeros(size(farEnd));
+estimatedEchoPath = zeros(size(farEnd));
 
-for n = 1:length(far_end_signal)
-    farEndSample = far_end_signal(n);
-    echoSample = echo_signal(n);
+for n = 1:length(farEnd)
+    farEndSample = farEnd(n);
+    echoSample = echoSignal(n);
     
-    y = adapt_filter' * delayedFarEndSignal; % Output of adaptive filter
+    y = adaptiveFilter' * delayedFarEnd; 
     error = echoSample - y; % Error signal
     
-    adapt_filter = adapt_filter + step_size * conj(delayedFarEndSignal) * error; % Update filter weights
+    adaptiveFilter = adaptiveFilter + step_size * conj(delayedFarEnd) * error; % Update filter weights
     
     error_signal(n) = error; % Store error signal
-    estimatedEchoPath(n) = adapt_filter' * delayedFarEndSignal; % Estimate of echo path
+    estimatedEchoPath(n) = adaptiveFilter' * delayedFarEnd; % Estimate of echo path
     
-    % Shift the delayedFarEndSignal by 1 sample
-    delayedFarEndSignal = [farEndSample; delayedFarEndSignal(1:end-1)];
+    delayedFarEnd = [farEndSample; delayedFarEnd(1:end-1)];
 end
 
 % Compute the frequency response of the estimated FIR channel
-[H_est, w_est] = freqz(adapt_filter, 1, length(adapt_filter));
+[H_est, w_est] = freqz(adaptiveFilter, 1, length(adaptiveFilter));
 
 % Compute the frequency response of the given FIR system (Path)
 [H_path, w_path] = freqz(path, 1, length(path));
@@ -42,22 +41,22 @@ end
 % Plot the amplitude response
 figure;
 subplot(2, 1, 1);
-plot(w_est/pi, 20*log10(abs(H_est)));
+plot(w_est/pi, 20*log10(abs(H_est)),'LineWidth',2,Color="#77AC30");
 hold on;
-plot(w_path/pi, 20*log10(abs(H_path)));
+plot(w_path/pi, 20*log10(abs(H_path)),'LineWidth',2,color="#7E2F8E");
 hold off;
 title('Amplitude Response');
-xlabel('Normalized Frequency (\times\pi rad/sample)');
-ylabel('Magnitude');
-legend('Estimated FIR Channel', 'Given FIR System (Path)');
+xlabel('Normalized Frequency (*pi rad/sample)');
+ylabel('Amplitude (dB)');
+legend('Estimated FIR Channel', 'FIR System (Path)');
 
 % Plot the phase response
 subplot(2, 1, 2);
-plot(w_est/pi, unwrap(angle(H_est)));
+plot(w_est/pi, unwrap(angle(H_est)),'LineWidth',2,Color="#77AC30");
 hold on;
-plot(w_path/pi, unwrap(angle(H_path)));
+plot(w_path/pi, unwrap(angle(H_path)),'LineWidth',2,color="#7E2F8E");
 hold off;
 title('Phase Response');
-xlabel('Normalized Frequency (\times\pi rad/sample)');
+xlabel('Normalized Frequency (*pi rad/sample)');
 ylabel('Phase (rad)');
-legend('Estimated FIR Channel', 'Given FIR System (Path)');
+legend('Estimated FIR Channel', 'FIR System (Path)');
